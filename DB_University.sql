ec84453f68ec74653d6b05e6de0e70b0704e5ddd
MonicaIzzo <info@monicaizzo.net>
2023-08-04 06:33:04 +0200

    1 EX - Query con GROUP BY

--- DB_University.sql
+++ DB_University.sql
@@ -37,22 +37,25 @@ FROM `degrees`
 WHERE `level` = `magistrale`
 
 -- 7.	Da quanti dipartimenti è composta l'università? (12)
-SELECT COUNT (*) AS `numero dipartimenti`
+SELECT COUNT (*) AS `numero_dipartimenti`
 FROM `departments`;
 
 
 -- 8.	Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT *
-FROM ``
-``
+SELECT COUNT(*) AS `num_insegnanti`
+FROM `teachers`
+WHERE `phone` IS NULL;
+
+
 
 
 
 EX - Query con GROUP BY
 -- 1.	Contare quanti iscritti ci sono stati ogni anno
-SELECT *
-FROM ` `
-``
+SELECT COUNT(*) AS `numero_studenti`, YEAR(`enrolment_date`) AS `anno`
+FROM `students`
+GROUP BY (`anno`);
+
 
 -- 2.	Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT *
