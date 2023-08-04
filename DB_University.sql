ba1f8c7e0b1bff5e301d5f472b6684970888a757
MonicaIzzo <info@monicaizzo.net>
2023-08-04 07:18:57 +0200

    start 3 ex

--- DB_University.sql
+++ DB_University.sql
@@ -71,3 +71,44 @@ GROUP BY `exam_id`;
 SELECT COUNT(`name`) AS `Corso`, `department_id` 
 FROM `degrees`
 GROUP BY `department_id`
+
+
+
+
+EX - Query con JOIN
+-- 1.	Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) = 1990;
+
+
+-- 2.	Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) = 1990;
+
+-- 3.	Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) = 1990;
+
+-- 4.	Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) = 1990;
+
+-- 5.	Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) = 1990;
+
+-- 6.	6.	Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) = 1990;
+
+
+-- 7.	BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) = 1990;
