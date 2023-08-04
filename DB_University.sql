8237205bd785594d078b4e02f2c7254462b449b3
MonicaIzzo <info@monicaizzo.net>
2023-08-04 06:39:14 +0200

    2 EX - Query con GROUP BY

--- DB_University.sql
+++ DB_University.sql
@@ -54,13 +54,13 @@ EX - Query con GROUP BY
 -- 1.	Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(*) AS `numero_studenti`, YEAR(`enrolment_date`) AS `anno`
 FROM `students`
-GROUP BY (`anno`);
+GROUP BY `anno`;
 
 
 -- 2.	Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT *
-FROM `students `
-``
+SELECT COUNT(*) AS `num_insegnanti`, `office_address` AS `edificio`
+FROM `teachers`
+GROUP BY `edificio`;
 
 -- 3.	Calcolare la media dei voti di ogni appello d'esame
 SELECT *
